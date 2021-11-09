a24e9896111c544f5b721c75ecb3c42faf6b490a
HongKaiJun <kaijun2110@gmail.com>
2021-11-09 17:04:50 +0800

    first commit

---
+++ src/main/java/pha/phaspring/PhaSpringApplication.java
@@ -0,0 +1,13 @@
+package pha.phaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pha/phaspring/PhaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pha.phaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
