1d89eb316f32903ae534c3a1b246de671bcbefd3
chandran.linesh <chandran.linesh@sicmsb.com>
2021-11-10 10:29:29 +0800

    model and create repo for edu-details

--- src/main/java/pha/phaspring/Model/EducationDetails.java
+++ src/main/java/pha/phaspring/Model/EducationDetails.java
@@ -1,5 +1,70 @@
 package pha.phaspring.Model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "educationdetails")
 public class EducationDetails {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "EmpEdu")
+    private int EmpEdu;
+
+    @Column(name = "CollegeName")
+    private String CollegeName;
+
+    @Column(name = "YearObtain")
+    private String YearObtain;
+
+    @Column(name = "Certificate")
+    private String Certificate;
+
+    public EducationDetails() {
+    }
+
+    public EducationDetails(int empEdu, String collegeName, String yearObtain, String certificate) {
+        EmpEdu = empEdu;
+        CollegeName = collegeName;
+        YearObtain = yearObtain;
+        Certificate = certificate;
+    }
+
+    public int getEmpEdu() {
+        return EmpEdu;
+    }
+
+    public void setEmpEdu(int empEdu) {
+        EmpEdu = empEdu;
+    }
+
+    public String getCollegeName() {
+        return CollegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        CollegeName = collegeName;
+    }
+
+    public String getYearObtain() {
+        return YearObtain;
+    }
+
+    public void setYearObtain(String yearObtain) {
+        YearObtain = yearObtain;
+    }
+
+    public String getCertificate() {
+        return Certificate;
+    }
+
+    public void setCertificate(String certificate) {
+        Certificate = certificate;
+    }
+
 }
---
+++ src/main/java/pha/phaspring/Repository/EducationDetailsRepository.java
@@ -0,0 +1,9 @@
+package pha.phaspring.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pha.phaspring.Model.EducationDetails;
+
+public interface EducationDetailsRepository extends JpaRepository<EducationDetails, Integer> {
+
+}
