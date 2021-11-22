0a6e998f0d1c70555f9754130937765b4b89a0e8
HongKaiJun <kaijun2110@gmail.com>
2021-11-22 11:00:56 +0800

    adding cross origin for data transfer

--- src/main/java/pha/phaspring/Controller/EmployeeController.java
+++ src/main/java/pha/phaspring/Controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import pha.phaspring.Response.EmployeeResponse;
 import pha.phaspring.Service.EmployeeService;
 
 
-
+@CrossOrigin(origins ="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
